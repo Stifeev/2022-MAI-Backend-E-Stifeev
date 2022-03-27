0e78918662606dca05deeebb10c0ea6eca53dd6a
Stifeev <55450005+Stifeev@users.noreply.github.com>
2022-03-27 21:27:55 +0300

    Update cache.py

--- ЛР1/cache.py
+++ ЛР1/cache.py
@@ -5,16 +5,6 @@ Created on Wed Feb 23 21:39:31 2022
 @author: stife
 """
 
-from time import time_ns
-
-class Node:
-    def __init__(self, value, time):
-        self.value = value # значение
-        self.time = time   # время запроса
-        
-    def __str__(self):
-        return "(%s, %d)" % (self.value, self.time)
-
 class LRUCache:
     def __init__(self, capacity: int=10) -> None:
         """
@@ -25,33 +15,31 @@ class LRUCache:
             capacity >= 1. Значение по умолчанию 10.
         """
         self.capacity = capacity
-        self.data = {}  # словарь: key = запрос, value - (значение, время запроса)
+        self.data = {}  # словарь: key = запрос, value - значение
     
     def get(self, key: str) -> str:
         if key in self.data:
-            self.data[key].time = time_ns()
-            return self.data[key].value
+            val = self.data[key][:]
+            del self.data[key]
+            self.data[key] = val
+            return val
         else:
             return ""
 
     def set(self, key: str, value: str) -> None:
-        if key in self.data:
-            self.data[key] = Node(value, time_ns())
+        if key in self.data: # уже есть
+            del self.data[key]
+            self.data[key] = value
         else:
             if len(self.data) + 1 > self.capacity: # проверка на переполнение
                 # удаляем последнее значение (в порядке добавление)
-                skey = None
-                smin = time_ns()
-                for _key, val in self.data.items(): # поиск минимума за линию
-                    time = val.time
-                    if time <= smin:
-                        smin = time
-                        skey = _key
-                del self.data[skey]
-                    
-            self.data[key] = Node(value, time_ns())
+                del self.data[next(iter(self.data.keys()))]
+            self.data[key] = value
             
     def rem(self, key: str) -> None:
         if key in self.data:
             del self.data[key]
+            
+    def __str__(self):
+        return "capacity = %d, data = %s" % (self.capacity, self.data)
         
