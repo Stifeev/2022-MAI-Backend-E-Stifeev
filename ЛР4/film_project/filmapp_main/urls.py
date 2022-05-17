42301e3423f444d1807e69a89e1e36905b177a22
Stifeev <55450005+Stifeev@users.noreply.github.com>
2022-05-17 21:49:30 +0300

    Update urls.py

--- ЛР4/film_project/filmapp_main/urls.py
+++ ЛР4/film_project/filmapp_main/urls.py
@@ -14,14 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path
 from filmapp import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    re_path(r'^jango/{0,1}$', views.index, name='home'),
+    path("", views.index, name='home'),
+    path("jango/", views.index, name='home'),
     
-    re_path(r"[jango/]{0,1}films/", views.products),
+    path("jango/films/", views.products),
+    path("films/", views.products),
     
     path('admin/', admin.site.urls),
 ]
